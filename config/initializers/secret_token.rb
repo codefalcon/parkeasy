09dabb5eaa25cb53ddba15193e7863387d7d750f
codefalcon <codefalcon@server.fake>
2013-10-18 16:45:43 +0530

    Project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkeasy::Application.config.secret_token = '381709303427cefca2267a70e37580b4daba8fe8d20b17c2075405f292628bf0d903aa3eb97a7c4e0adbec970fc0a2ff648317cdf1de73e3605f5363acabab9e'
