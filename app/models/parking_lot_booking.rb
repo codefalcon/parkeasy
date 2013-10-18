cae489cd470711272aef0aad28b08501393ab3b3
Balakrishna Jammula <balakrishnaj@globalscholar.com>
2013-10-19 03:59:18 +0530

    Parking lot booking controller - Balaji

--- app/models/parking_lot_booking.rb
+++ app/models/parking_lot_booking.rb
@@ -2,4 +2,13 @@ class ParkingLotBooking < ActiveRecord::Base
   belongs_to :user
   belongs_to :parking_space_lot
   attr_accessible :booking_type_id, :end_date, :end_time, :number_of_lots_booked, :payment_amount, :payment_reference, :start_date, :start_time
+  def BookingType
+    booking_type_id = BookingType.BookingTypes().select { |key, value| value == self.booking_type_id }
+    
+    if booking_type_id.nil? 
+      return nil 
+    else 
+      return booking_type_id[0][0] 
+    end
+  end
 end
--- app/models/parking_space_lot.rb
+++ app/models/parking_space_lot.rb
@@ -1,4 +1,23 @@
 class ParkingSpaceLot < ActiveRecord::Base
   belongs_to :parking_space
   attr_accessible :allowed_vehicle_type_id, :end_date, :end_time, :number_of_lots, :preferred_duration_type_id, :start_date, :start_time
+  def AllowedVehicleType
+    allowed_vehicle_type_id = AllowedVehicleType.AllowedVehicleTypes().select { |key, value| value == self.allowed_vehicle_type_id }
+    
+    if allowed_vehicle_type_id.nil? 
+      return nil 
+    else 
+      return allowed_vehicle_type_id[0][0] 
+    end
+  end
+  def PreferredDurationType
+    preferred_duration_type_id = PreferredDurationType.PreferredDurationTypes().select { |key, value| value == self.preferred_duration_type_id }
+    
+    if preferred_duration_type_id.nil? 
+      return nil 
+    else 
+      return preferred_duration_type_id[0][0] 
+    end
+  end  
+
 end
